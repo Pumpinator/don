cf9c78244b9070e2735d2ccf15dde4dc20cb06c2
Pumpinator <alejandrodcardona5@gmail.com>
2024-11-27 21:02:39 -0600

    explicacion mejorada de las tecnologías para el equipo de desarrollo

--- src/main/java/edu/mx/utleon/dongalleto/controller/MainController.java
+++ src/main/java/edu/mx/utleon/dongalleto/controller/MainController.java
@@ -1,5 +1,6 @@
 package edu.mx.utleon.dongalleto.controller;
 
+import edu.mx.utleon.dongalleto.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,14 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("clave", "valor");
-        model.addAttribute("arreglo", new String[] { "a", "b", "c" });
-        model.addAttribute("lista", List.of("a", "b", "c"));
+        model.addAttribute("key", "value"); // Enviar un atributo a la vista
+        model.addAttribute("array", new String[] { "a", "b", "c" }); // Enviar un arreglo a la vista
+        model.addAttribute("list", List.of("a", "b", "c")); // Enviar una lista a la vista
+        model.addAttribute("number", 123); // Enviar un número a la vista
+        model.addAttribute("boolean", true); // Enviar un booleano a la vista
+        model.addAttribute("product", Product.builder().id(22L).name("Galleta de Nuez").description("Los ingredientes son nuez, harina, huevo y azúcar").build()); // Enviar un objeto a la vista
         return "index";
     }
 
 }
+
