1a343416ac0b9d46a5aaad5c723f31d15ea5047b
JosueChavez1 <josuechavez0312@gmail.com>
2024-12-04 19:51:17 -0600

    se agrego produccion

--- src/main/java/edu/mx/utleon/dongalleto/controller/KitchenController.java
+++ src/main/java/edu/mx/utleon/dongalleto/controller/KitchenController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,6 +23,13 @@ public class KitchenController {
         model.addAttribute("ingredients", kitchenService.getIngredients());
         model.addAttribute("measures", kitchenService.getMeasures());
         return "kitchen/index";
+    }
+
+    @GetMapping
+    @RequestMapping("/produccion")
+    public String ejemplo(Model model) {
+        model.addAttribute("variable", "Soy Brandon y soy gay!!!!!");
+        return "kitchen/production";
     }
 
 }
