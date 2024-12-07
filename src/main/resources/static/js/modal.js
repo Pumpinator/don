1a9c17642231882f2a54d8de13bb4bb3c848d0fb
skkinydud3 <montoyabrandon386@gmail.com>
2024-12-06 18:42:47 -0600

    modal de recetas

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,9 +1,11 @@
 function openModal() {
     document.getElementById('modal').classList.remove('hidden');
     document.body.classList.add('modal-open');
+    document.getElementById('mainContent').classList.add('blur');
 }
 
 function closeModal() {
     document.getElementById('modal').classList.add('hidden');
     document.body.classList.remove('modal-open');
+    document.getElementById('mainContent').classList.remove('blur');
 }
