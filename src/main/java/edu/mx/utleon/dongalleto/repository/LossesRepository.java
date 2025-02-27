46e951df3bb1ac39fc8092551368ff162146ce0a
Pumpinator <alejandrodcardona5@gmail.com>
2025-02-27 08:19:32 -0600

    primer commit, versión en Python del proyecto

--- src/main/java/edu/mx/utleon/dongalleto/repository/LossesRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mx.utleon.dongalleto.repository;
-
-import edu.mx.utleon.dongalleto.model.Losses;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LossesRepository extends CrudRepository<Losses, Integer> {
-}
--- src/main/java/edu/mx/utleon/dongalleto/repository/SaleDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mx.utleon.dongalleto.repository;
-
-import edu.mx.utleon.dongalleto.model.SaleDetail;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleDetailRepository extends CrudRepository<SaleDetail, Integer> {
-}
