d36d48b19219b94ae0a156ab39d3c763ad2bb1f5
Pumpinator <alejandrodcardona5@gmail.com>
2025-03-04 14:51:22 -0600

    layout, colores y estructura de carpetas

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route('/compras')
+def compras():
+    return render_template('compras.html')
+
+@app.route('/insumos')
+def insumos():
+    return render_template('insumos.html')
+
+@app.route('/recetas')
+def recetas():
+    return render_template('recetas.html')
+
+@app.route('/galletas')
+def galletas():
+    return render_template('galletas.html')
+
+@app.route('/mermas')
+def mermas():
+    return render_template('mermas.html')
+
+@app.route('/produccion')
+def produccion():
+	return render_template('produccion.html')
+
+@app.route('/ventas')
+def ventas():
+    return render_template('ventas.html')
+
+@app.route('/reportes')
+def reportes():
+	return render_template('reportes.html')
+
+if __name__ == '__main__':
+	app.run(debug=True, port=3000)
