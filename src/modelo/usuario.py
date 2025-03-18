525a5ea698714c2808c12c764845c96699952496
IDGS-805-22001761 <78649@alumnos.utleon.edu.mx>
2025-03-18 17:26:19 -0600

    correccion usuarios

--- src/modelo/usuario.py
+++ src/modelo/usuario.py
@@ -1,14 +1,10 @@
 from bd import bd
-from modelo.rol import Rol
 
 class Usuario(bd.Model):
     __tablename__ = 'usuarios'
-    
     id = bd.Column(bd.Integer, primary_key=True)
-    nombre = bd.Column(bd.String(150), nullable=False)
-    password = bd.Column(bd.String(50), nullable=False)
-    rol_id = bd.Column(bd.Integer, bd.ForeignKey('roles.id'), nullable=False)
-    email = bd.Column(bd.String(50), nullable=False)
-    
-    rol_id = bd.Column(bd.Integer, bd.ForeignKey('roles.id'), nullable=False)
-    rol = bd.relationship(Rol, backref='usuarios')
+    nombre = bd.Column(bd.String, nullable=False)
+    email = bd.Column(bd.String, nullable=False, unique=True)
+    password = bd.Column(bd.String, nullable=False)
+    rol_id = bd.Column(bd.Integer, nullable=False)
+    estatus = bd.Column(bd.String, nullable=False)
--- src/servicio/usuario.py
+++ src/servicio/usuario.py
@@ -2,3 +2,8 @@ from modelo.usuario import Usuario
 
 def obtener_usuarios():
     return Usuario.query.all()
+
+def crear_usuario(nombre, correo, contrasena):
+    usuario = Usuario(nombre=nombre, correo=correo, contrasena=contrasena)
+    usuario.save()
+    return usuario
