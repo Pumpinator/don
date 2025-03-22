7b63220eb9983445a276b8f170ed1afa5fa53a5e
Vayron-hub <vayron15conchas@gmail.com>
2025-03-22 08:38:29 -0600

    insumos con bd pendiente de terminar

--- src/controlador/insumo.py
+++ src/controlador/insumo.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
-from servicio.insumo import obtener_insumos
+from servicio.insumo import InsumoServicio
+from bd import bd
 
 controlador = Blueprint('controlador_insumo', __name__)
 
 @controlador.route('/insumos')
 def insumos():
-    insumos = obtener_insumos()
-    return render_template('insumos.html', insumos=insumos)
+    insumoservicio = InsumoServicio( bd )
+    insumos = insumoservicio.obtener_insumos()
+    inventario = insumoservicio.obtener_inventario()
+    return render_template('insumos.html', insumos=insumos, inventario=inventario)
--- src/servicio/insumo.py
+++ src/servicio/insumo.py
@@ -1,4 +1,13 @@
+from modelo.insumo_inventario import InsumoInventario
 from modelo.insumo import Insumo
 
-def obtener_insumos():
-    return Insumo.query.all()
+
+class InsumoServicio:
+    def __init__(self, bd):
+        self.bd = bd
+     
+    def obtener_insumos(self):
+        return self.bd.session.query(Insumo).all()
+    
+    def obtener_inventario(self):
+        return self.bd.session.query(InsumoInventario).all()
