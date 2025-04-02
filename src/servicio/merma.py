f3af3fbae237e2c1718a9320c708f97e8134bb6a
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-04-01 23:13:29 -0600

    Descuenta insumos en merma

--- src/servicio/merma.py
+++ src/servicio/merma.py
@@ -1,8 +1,12 @@
 from modelo.merma import Merma
 from modelo.produccion import Produccion
+from modelo.insumo import Insumo
+from modelo.galleta import Galleta
+from modelo.insumo_inventario import InsumoInventario  
+
 class MermaServicio:
     
-    def __init__ (self, bd):
+    def __init__(self, bd):
         self.bd = bd
         
     def obtener_mermas(self):
@@ -28,11 +32,27 @@ class MermaServicio:
 
         if tipo == "insumo":
             merma.insumo_id = item_id
+            
+            # Buscar el insumo en el inventario y restarle la cantidad de la merma
+            insumo_inventario = self.bd.session.query(InsumoInventario).filter_by(
+                insumo_id=item_id, medida_id=medida_id
+            ).first()
+
+            if not insumo_inventario:
+                raise ValueError("El insumo no se encuentra en el inventario.")
+            
+            if insumo_inventario.cantidad < cantidad:
+                raise ValueError("No hay suficiente cantidad en el inventario para registrar esta merma.")
+
+            insumo_inventario.cantidad -= cantidad  # Restar la merma del inventario
+
         elif tipo == "galleta":
             merma.galleta_id = item_id
+            # Resta cantidad de la galleta en el inventario si es necesario
+
         elif tipo == "produccion":
             merma.produccion_id = item_id
+            # Resta cantidad de la producción si es necesario
             
         self.bd.session.add(merma)
         self.bd.session.commit()
-        
