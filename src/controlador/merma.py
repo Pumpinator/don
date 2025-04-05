a2f76a5ed32593d8b676e1ee0132827b64d3a069
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-04-05 14:24:47 -0600

    Agregar mermas en formulario de produccion

--- src/controlador/merma.py
+++ src/controlador/merma.py
@@ -10,36 +10,53 @@ controlador = Blueprint('merma', __name__)
 
 admin_or_trabajador_permission = Permission(RoleNeed('ADMIN'), RoleNeed('TRABAJADOR'))
 
-# Mover la ruta de las mermas a un script de mermas distinto al principal
-@controlador.route('/mermas',methods=['GET','POST'])
+# Ruta para ver todas las mermas
+@controlador.route('/mermas', methods=['GET', 'POST'])
 @login_required
 @admin_or_trabajador_permission.require(http_exception=403)
 def mermas():
     merma_servicio = MermaServicio(bd)
-    mermas = merma_servicio.obtener_mermas() # Obtiene todas las mermas mediante un método del servicio
-    return render_template('merma/mermas.html',mermas=mermas)
+    mermas = merma_servicio.obtener_mermas()  # Obtiene todas las mermas mediante un método del servicio
+    return render_template('merma/mermas.html', mermas=mermas)
 
-# '/agregar_merma' nombre modificado a '/merma/agregar' para seguir la convención de rutas
+# Ruta para agregar una nueva merma
 @controlador.route('/merma/agregar', methods=['GET', 'POST'])
 @login_required
 @admin_or_trabajador_permission.require(http_exception=403)
 def agregar_merma():
+    produccion_id = request.args.get("produccion_id")
     if request.method == 'POST':
         try:
+            # Agregar la merma
             merma_servicio = MermaServicio(bd)
             merma_servicio.agregar_merma(request.form)
-            # Obtener el ID de la producción desde el formulario
-            produccion_id = request.form.get("produccion_id")
-            if produccion_id:
-                # Actualizar el estatus de la producción en la BD
+            
+            # Si se envió produccion_id en el formulario, se actualiza el estatus de la producción
+            if request.form.get("produccion_id"):
                 cocina_servicio = CocinaServicio(bd)
-                cocina_servicio.actualizar_estatus_produccion(produccion_id, 4)  # 4 = Mermado
+                cocina_servicio.actualizar_estatus_produccion(request.form.get("produccion_id"), 4)  # 4 = Mermado
+
+                # Eliminar la producción del select si es mermada
+                cocina_servicio.eliminar_produccion(request.form.get("produccion_id"))
+
+                # Descontar las galletas mermadas del inventario
+                galletas_merceadas = request.form.get("galletas_mermadas")
+                if galletas_merceadas:
+                    inventario_servicio = InventarioServicio(bd)
+                    inventario_servicio.descontar_galletas_del_inventario(galletas_merceadas)
+
             flash("Merma agregada exitosamente", "success")
-            return redirect(url_for('principal.merma.mermas'))
+            return redirect(url_for('principal.merma.mermas'))  # Redirige a la lista de mermas
+
         except ValueError as e:
             flash(str(e), "danger")
+
+    # Pasa los datos necesarios a la plantilla
     insumos = InventarioServicio(bd).obtener_insumos()  
     galletas = CocinaServicio(bd).obtener_galletas()
-    producciones = CocinaServicio(bd).obtener_producciones() 
+    producciones = CocinaServicio(bd).obtener_producciones()  # Obtiene las producciones
     medidas = CocinaServicio(bd).obtener_medidas()
-    return render_template('merma/agregar.html', insumos=insumos, galletas=galletas, producciones=producciones, medidas=medidas)
+
+    # Pasa las producciones para el select en la plantilla
+    return render_template('merma/agregar.html', insumos=insumos, galletas=galletas,
+                           producciones=producciones, medidas=medidas, produccion_id=produccion_id)
